4198886d37660d81ee34f694ae4e7ff35e25b250
Lana Araújo <lana.academico2@gmail.com>
2023-09-25 10:48:40 -0300

    alteracao2

---
+++ petshop/agendamento/consulta_agenda.php
@@ -0,0 +1,8 @@
+<?php
+CREATE TABLE cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    endereco VARCHAR(255) NOT NULL
+);
