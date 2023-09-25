705b97680b94160a8586972b61aa8f14c2c2ee13
Lana Araújo <lana.academico2@gmail.com>
2023-09-25 10:56:24 -0300

    alt3

--- petshop/index.php
+++ petshop/index.php
@@ -6,24 +6,6 @@
     <title>PET SHOP CADASTRO</title>
 </head>
 <body>
-<div class="content">
-    <h1>Cadastro de PETSHOP</h1>
-        <form action="cadastro_petshop.php" method="POST" class="formulario">
-        <label for="tipoanimal">Tipo do animal:</label>
-        <select name="tipoanimal" id="">
-                <option value="Cao">Cão</option>
-                <option value="gato">Gato</option>
-                <option value="tartaruga">Tartaruga</option>
-                <option value="passaro">Pássaro</option>
-            </select>
-            <label for="nome">Nome do seu animal:</label>
-            <input type="text" name="nome" id="" placeholder="Digite o nome do animal">
-            <label for="raca">Raça do seu animal:</label>
-            <input type="text" name="raca" id="" placeholder="Digite a raça do seu animal">
-            <input type="submit" value="Cadastrar" class="btn">
-        </form>
-        <button class="btn-voltar"><a href="lista_pacientes.php">Lista de Pacientes</a></button>
-    </div>
     
 </body>
 </html>
