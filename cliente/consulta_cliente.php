fcf4e3959929972d87f048adfb066aace1070660
Lana Araújo <lana.academico2@gmail.com>
2023-09-25 14:19:05 -0300

    alt 6

--- cliente/consulta_cliente.php
+++ cliente/consulta_cliente.php
@@ -1,31 +1,65 @@
 <?php
 include '../conexao.php';
 
-$sql = "SELECT nome, endereco FROM cliente";
-
+$sql = "SELECT * FROM cliente
+        INNER JOIN animal ON cliente_cpf = fk_cliente_cpf"
 $resultado = $mysqli->query($sql);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <title>Consulta de Clientes</title>
 </head>
 <body>
     <h1>Consulta de Clientes</h1>
     <table>
         <tr>
+            <th>CPF Cliente</th>
             <th>Nome</th>
             <th>Endereço</th>
+            <th>Excluir</th>
         </tr>
-        <?php
-        while ($row = $resultado->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["nome"] . "</td>";
-            echo "<td>" . $row["endereco"] . "</td>";
-            echo "</tr>";
-        }
-        ?>
+<?php
+if ($resultado->num_rows > 0) {
+    while ($row = $resultado->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["cliente_cpf"] . "</td>";
+        echo "<td>" . $row["cliente_nome"] . "</td>";
+        echo "<td>" . $row["cliente_endereco"] . "</td>";
+        echo "<td><a href='exclui_cliente.php id=" . $row["animal_cod"] . "'>Excluir</a></td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='5'>Nenhum cliente cadastrado.</td></tr>";
+}
+?>
+
     </table>
+    <style>
+        
+h1{
+    text-align: center;
+}
+
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    background-color: #e6f7ff; /* Cor de fundo azul claro */
+}
+
+th, td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+}
+
+tr:nth-child(even) {
+    background-color: #cce0ff; /* Cor de fundo ainda mais clara para linhas pares */
+}
+
+    </style>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,16 +1,88 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PET SHOP CADASTRO</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Página com Fundo Bonito e Imagens</title>
+    <style>
+        /* Estilização do corpo */
+        body {
+            background-image: url('https://png.pngtree.com/background/20210710/original/pngtree-cartoon-pet-footprints-simple-gray-banner-picture-image_1053918.jpg'); /* URL da sua imagem de fundo */
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            color: #333; /* Cor do texto */
+        }
+
+        /* Estilização do cabeçalho */
+        header {
+            background-color: #f8f8f8; /* Cor de fundo do cabeçalho */
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            color: #333; /* Cor do texto do cabeçalho */
+        }
+
+        /* Estilização do conteúdo */
+        .content {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: rgba(255, 255, 255, 0.9); /* Cor de fundo com transparência */
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); /* Sombra */
+        }
+
+        /* Estilização das imagens */
+        img {
+            max-width: 100%;
+            height: auto;
+            display: block;
+            margin: 20px auto; /* Espaçamento entre as imagens */
+        }
+        /* style.css */
+/* Estilização para botões */
+.button-container {
+    text-align: center;
+    margin-top: 20px;
+}
+
+.button {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #0077cc; /* Cor de fundo dos botões */
+    color: #fff; /* Cor do texto dos botões */
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease; /* Efeito de transição ao passar o mouse */
+    margin-right: 10px; /* Espaço entre os botões */
+}
+
+.button:hover {
+    background-color: #005299; /* Cor de fundo dos botões ao passar o mouse */
+}
+
+        
+    </style>
 </head>
 <body>
-    <h1>PET SHOP</h1>
-    
-
-    
+    <header>
+        <h1>Página com Fundo Bonito e Imagens</h1>
+    </header>
+    <div class="content">
+        <p>Bem vindo ao nosso PETSHOP!</p>
+        <div class="button-container">
+        <a href="agendamento/consulta_agenda.php" class="button">Consultar Agenda</a>
+        <a href="animal/consulta_animal.php" class="button">Consultar Animal</a>
+        <a href="consulta/consulta_cliente.php" class="button">Consultar Cliente</a>
+    </div>
+        <img src="https://idec.org.br/sites/default/files/dicasedireitos/como-escolher-petshop.jpg" alt="Imagem 1">
+    </div>
 </body>
 </html>
